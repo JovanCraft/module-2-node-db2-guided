db4e84ba05c0004a58ade59d564f63a42d7e74cc
jovan <jovancraft@gmail.com>
2024-01-31 12:44:15 -0600

    built the id, name, weight, and taste columns of my database over agin

--- data/migrations/20240131180014_first-migration.js
+++ data/migrations/20240131180014_first-migration.js
@@ -2,14 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable('fruits', table => {
+        table.increments('fruits_id')//this is the schortcut to created primary ID's
+        table.string('fruit_name', 100).notNullable().unique()//.unique makes it where you can't have 2 pieces of data with the same name
+        table.float('avg_weight_oz').notNullable()//can use float or decimal
+        //notNullable means that to add another record to the table, you HAVE to provide those criterias
+        //--production data base is NEVER migrated down because it destroys data. Only development DB is migrated down!
+        table.boolean('delicious')
+    })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('fruits')
 };
