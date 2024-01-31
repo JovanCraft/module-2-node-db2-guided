00c691df897b621149aca6a13578ccdb0ad3596a
jovan <jovancraft@gmail.com>
2024-01-31 13:39:27 -0600

    finished getting the color coumn set up

--- data/seeds/fruits.js
+++ data/seeds/fruits.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('fruits').truncate()//truncate resets primamry keys
   await knex('fruits').insert([
-    {fruit_name: 'apple', avg_weight_oz: 12.5, delicious: null},
-    {fruit_name: 'orange', avg_weight_oz: 4.03, delicious: true},//sqlite does NOT have boolean but it is ok to put them because it gets changed in the database into an integer: 1 = TRUE and 0 = FALSE!!
-    {fruit_name: 'pear', avg_weight_oz: 10.2, delicious: 0},
+    {fruit_name: 'apple', avg_weight_oz: 12.5, delicious: null, color: 'red'},
+    {fruit_name: 'orange', avg_weight_oz: 4.03, delicious: true, color: 'orange'},//sqlite does NOT have boolean but it is ok to put them because it gets changed in the database into an integer: 1 = TRUE and 0 = FALSE!!
+    {fruit_name: 'pear', avg_weight_oz: 10.2, delicious: 0, color: 'green'},
   ]);//we NEVER write IDs. We let the database do it FOR US
 };
